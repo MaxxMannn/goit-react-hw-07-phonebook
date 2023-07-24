30d075dcdbac800bc4938fc2c6abb5a7a503d35f
MaxxMannn <116672250+MaxxMannn@users.noreply.github.com>
2023-07-24 18:00:40 +0200

    p

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import ContactForm from './ContactsForm/ContactForm'
+import Filter from './Filter/Filter'
+import ContactsList from './ContactsList/ContactsList'
+import {Container} from './AppStyled'
+import { useSelector } from 'react-redux'
+
+const NewApp = () => {
+
+  const contactsList = useSelector(state=>state.contacts)
+
+      return(
+        <Container>
+        <h1>PhoneBook</h1>
+        <ContactForm
+        />
+        
+        <h2>Contacts</h2>
+        <Filter
+        />
+        {contactsList && 
+        <ContactsList
+        /> }
+        </Container>
+    )
+}
+
+export default NewApp;
